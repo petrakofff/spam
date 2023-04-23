8c81fbe5c84aaa9207c3d66709ba680e9b71e996
petrakofff <121085978+petrakofff@users.noreply.github.com>
2023-04-23 15:45:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-import pickle
 import streamlit as st
-from sklearn.externals import joblib
-
-# Load the pre-trained model
-spam_classifier = joblib.load('spam_classifier.pkl')
+import pickle
+import pandas as pd
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.snowball import SnowballStemmer
+from sklearn.feature_extraction.text import CountVectorizer
 
 # Загрузка обученной модели из файла
-with open('spam_classifier.pkl', 'rb') as f:
+with open('LR.pkl', 'rb') as f:
     model = pickle.load(f)
 
 # Определение функции для классификации ссылки
 def classify_link(link):
-    prediction = model.predict([link])
+    tokenizer = RegexpTokenizer(r'[A-Za-z]+')
+    stemmer = SnowballStemmer("english")
+    cv = CountVectorizer()
+
+    link_tokenized = tokenizer.tokenize(link)
+    link_stemmed = [stemmer.stem(word) for word in link_tokenized]
+    link_sent = ' '.join(link_stemmed)
+
+    features = cv.fit_transform([link_sent])
+    prediction = model.predict(features)
     if prediction[0] == 1:
         return "Спам"
     else:
